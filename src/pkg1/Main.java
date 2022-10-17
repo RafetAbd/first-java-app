a181d4d1209ce96841130d522641a6e41f53df33
Rafet Abdalgalil <rafetalani@gmail.com>
2022-10-17 14:29:16 -0500

    data types in java

--- src/pkg1/Main.java
+++ src/pkg1/Main.java
@@ -4,5 +4,13 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
+        int num1 = 32435;
+        long num2 = 329875309;
+        double num3 = 8456.9475;
+        char ch1 = 'A';
+        boolean b2 = true;
+        String name = "sgdg";
+
+        System.out.println(num2);
     }
 }
