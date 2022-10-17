7dd263b007307bb8dc4bde456b13ae6f0b61525e
Rafet Abdalgalil <rafetalani@gmail.com>
2022-10-17 14:59:16 -0500

    conditions in java

--- src/pkg1/Main.java
+++ src/pkg1/Main.java
@@ -3,14 +3,12 @@ package pkg1;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
-        int num1 = 32435;
-        long num2 = 329875309;
-        double num3 = 8456.9475;
-        char ch1 = 'A';
-        boolean b2 = true;
-        String name = "sgdg";
-
-        System.out.println(num2);
+        int num1 = 43533;
+        int num2 = 9843;
+        if ( num1 > num2 ) {
+            System.out.println("num one is bigger");
+        } else {
+            System.out.println("num two is bigger");
+        }
     }
 }
