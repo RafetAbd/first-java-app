2d865f9be976519b7ff7159f2a1c3edb08f10dae
Rafet Abdalgalil <rafetalani@gmail.com>
2022-10-17 15:23:06 -0500

    simple fabonacci approach

--- src/pkg1/Main.java
+++ src/pkg1/Main.java
@@ -3,20 +3,16 @@ package pkg1;
 public class Main {
     public static void main(String[] args) {
 
-        int mark = 64;
 
-        if ( mark >= 90 ) {
-            System.out.println("Grade A");
-        } else if ( mark >= 80 ) {
-            System.out.println("Grade B");
-        } else if ( mark >= 70 ) {
-            System.out.println("Grade c");
-        } else if ( mark >= 60 ) {
-            System.out.println("Grade d");
-        } else if ( mark >= 50 ) {
-            System.out.println("Grade E");
-        } else {
-            System.out.println("failed");
+        // fabonacci simple approach;
+       int num1 = 0;
+       int num2 = 1;
+       int num3 = 0;
+        for ( int i = 1 ; i <= 18 ; i ++ ) {
+            num3 = num1+num2;
+            System.out.println(num3);
+            num1 = num2;
+            num2 = num3;
         }
     }
 }
