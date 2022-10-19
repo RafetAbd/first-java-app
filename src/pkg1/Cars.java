9227b02e09c095c265355252d45e13e532d47496
Rafet Abdalgalil <rafetalani@gmail.com>
2022-10-18 19:10:04 -0500

    add car class

---
+++ src/pkg1/Cars.java
@@ -0,0 +1,13 @@
+package pkg1;
+
+public class Cars {
+
+    String color;
+    String make;
+    String type;
+
+    public void displayDetails() {
+        System.out.println("I have a "+make+" "+" "+type+" "+" of "+" "+ color+" color");
+    }
+
+}
--- src/pkg1/Main.java
+++ src/pkg1/Main.java
@@ -5,14 +5,31 @@ public class Main {
 
 
         // fabonacci simple approach;
-       int num1 = 0;
-       int num2 = 1;
-       int num3 = 0;
-        for ( int i = 1 ; i <= 18 ; i ++ ) {
-            num3 = num1+num2;
-            System.out.println(num3);
-            num1 = num2;
-            num2 = num3;
-        }
+//       int num1 = 0;
+//       int num2 = 1;
+//       int num3 = 0;
+//        for ( int i = 1 ; i <= 18 ; i ++ ) {
+//            num3 = num1+num2;
+//            System.out.println(num3);
+//            num1 = num2;
+//            num2 = num3;
+//        }
+
+//        int a = 100;
+//        while ( a > 50 ) {
+//            if ( a == 70 ) {
+//                continue;
+//            }
+//            System.out.println(a);
+//            a -= 5;
+//
+//        }
+
+        Cars car1 = new Cars();
+        car1.make = "Ford";
+        car1.type = "Edge";
+        car1.color = "Black";
+        car1.displayDetails();
+
     }
 }
