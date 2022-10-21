efbafe3029598ee6b66405b2f1f62d40bb53b19e
Rafet Abdalgalil <rafetalani@gmail.com>
2022-10-20 19:26:40 -0500

    modified bank account

--- src/pkg2/Bank_Account.java
+++ src/pkg2/Bank_Account.java
@@ -4,37 +4,35 @@ public class Bank_Account {
 
     private int AccountNumber;
     private String AccountName;
-    private int AccountBalance;
+    private double AccountBalance;
 
-    public Bank_Account( int AccountNumber, String AccountName, int AccountBalance) {
+    public Bank_Account( int AccountNumber, String AccountName, double AccountBalance) {
         this.AccountBalance = AccountBalance;
         this.AccountNumber = AccountNumber;
         this.AccountName = AccountName;
 
     }
 
-    public int getAccountBalance() {
-        return AccountBalance;
+    public void deposite(double addedMoney) {
+        if ( addedMoney <= 0 ) {
+            System.out.println("can not deposite " + addedMoney);
+        } else {
+            AccountBalance = AccountBalance + addedMoney;
+            System.out.println("the new balance is $" + AccountBalance);
+        }
     }
 
-    public void setAccountBalance(int accountBalance) {
-        AccountBalance = accountBalance;
-    }
-
-    public int deposite(int addedMoney) {
-        int newAccountBalance = AccountBalance + addedMoney;
-        setAccountBalance(newAccountBalance);
-        return getAccountBalance();
-    }
-
-    public int withdraw(int addedMoney) {
-        int newAccountBalance = AccountBalance - addedMoney;
-        setAccountBalance(newAccountBalance);
-        return getAccountBalance();
+    public void withdraw(double withdrawMoney) {
+        if ( withdrawMoney <= 0 || AccountBalance < withdrawMoney) {
+            System.out.println("can not deposite " + withdrawMoney);
+        } else {
+            AccountBalance = AccountBalance + withdrawMoney;
+            System.out.println("the new balance is $" + AccountBalance);
+        }
     }
 
     public void display() {
-        System.out.println("the current balance is $" + getAccountBalance());
+        System.out.println("the current balance is $" + AccountBalance);
     }
 
 }
