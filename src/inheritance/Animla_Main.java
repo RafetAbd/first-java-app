dc690986127f7800ead259190adf08ebef3d7962
Rafet Abdalgalil <rafetalani@gmail.com>
2022-10-22 11:13:21 -0500

    polymorphism in java

--- src/inheritance/Animla_Main.java
+++ src/inheritance/Animla_Main.java
@@ -4,11 +4,18 @@ public class Animla_Main {
 
     public static void main(String[] args) {
 
+        Animal a;
+
         Dog d = new Dog();
         d.whoAmI();
 
         Cat c = new Cat();
         c.whoAmI();
+
+        // classic example of polymorphisim :
+        a = new Dog();
+        a.whoAmI();
+        // a took the an object shape of Dog class.
 
     }
 
