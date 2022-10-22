2709ca1a7e7833ce0333eb6d957d5712b865b7cf
Rafet Abdalgalil <rafetalani@gmail.com>
2022-10-22 11:04:58 -0500

    method override 2

---
+++ src/inheritance/Animal.java
@@ -0,0 +1,9 @@
+package inheritance;
+
+public class Animal {
+
+    public void whoAmI() {
+        System.out.println("I am an Animal");
+    }
+
+}
---
+++ src/inheritance/Animla_Main.java
@@ -0,0 +1,15 @@
+package inheritance;
+
+public class Animla_Main {
+
+    public static void main(String[] args) {
+
+        Dog d = new Dog();
+        d.whoAmI();
+
+        Cat c = new Cat();
+        c.whoAmI();
+
+    }
+
+}
